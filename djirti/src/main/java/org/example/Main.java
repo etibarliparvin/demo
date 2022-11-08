ecf9cf890b5c83b6e89181947aa6d3bc47f15f63
nizam <nizam.novruzov@caspiantech.az>
2022-11-08 11:07:29 +0400

    nizam

--- djirti/src/main/java/org/example/Main.java
+++ djirti/src/main/java/org/example/Main.java
@@ -6,7 +6,11 @@ public class Main {
         System.out.println("Salam Nizam");
     }
 
+    public static void foo2() {
+        System.out.println("foo2");
+    }
+
     public static void main(String[] args) {
-        foo();
+        foo2();
     }
 }
